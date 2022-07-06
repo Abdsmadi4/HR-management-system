bec23801db0932114c897845f2fd8ba1a6e6c629
Abdsmadi4 <abd.smadi41@gmail.com>
2022-07-06 18:10:02 +0300

    add assets folder and emploee image

--- app.js
+++ app.js
@@ -43,13 +43,13 @@ Employee.prototype.render = function(){
 }
 
 //creat objects
-const emp1 = new Employee(1000,'Ghazi','Samer','Administration','Senior','');
-const emp2 = new Employee(1001,'Lana','Ali','Finance','Senior','');
-const emp3 = new Employee(1002,'Tamara','Ayoub','Marketing','Senior','');
-const emp4 = new Employee(1003,'Safi','Walid','Administration','Mid-Senior','');
-const emp5 = new Employee(1004,'Omar','Zaid','Development','Senior','');
-const emp6 = new Employee(1005,'Rana','Saleh','Development','Junior','');
-const emp7 = new Employee(1006,'Hadi','Ahmad','Finance','Mid-Senior','');
+const emp1 = new Employee(1000,'Ghazi','Samer','Administration','Senior','./assets/Ghazi.jpg');
+const emp2 = new Employee(1001,'Lana','Ali','Finance','Senior','./assets/Lana.jpg');
+const emp3 = new Employee(1002,'Tamara','Ayoub','Marketing','Senior','./assets/Tamara.jpg');
+const emp4 = new Employee(1003,'Safi','Walid','Administration','Mid-Senior','./assets/Safi.jpg');
+const emp5 = new Employee(1004,'Omar','Zaid','Development','Senior','./assets/Omar.jpg');
+const emp6 = new Employee(1005,'Rana','Saleh','Development','Junior','./assets/Rana.jpg');
+const emp7 = new Employee(1006,'Hadi','Ahmad','Finance','Mid-Senior','./assets/Hadi.jpg');
 
 
 //invoke prototype functions
